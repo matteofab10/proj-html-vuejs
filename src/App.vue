d1062bd3cfce456d1ae12c7570550e78a53dcafe
matteofab10 <matteofabio10@gmail.com>
2021-12-13 15:51:22 +0100

    struttura base

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    <header>HEADER</header>
-
-    <main>MAIN</main>
-
-    <footer>FOOTER</footer>
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
-
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
 }
 
 </script>
 
 <style lang="scss">
+@import "./assets/style/generals.scss";
+@import url('https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,400;0,700;1,400&display=swap');
 
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <section>
+      SECTION 1
+    </section>
+    <section>
+      SECTION 2
+    </section>
+    <section>
+      SECTION 3
+    </section>
+    <section>
+      SECTION 4
+    </section>
+    <section>
+      SECTION 5
+    </section>
+    <section>
+      SECTION 6
+    </section>
+    <section>
+      SECTION 7
+    </section>
+    <section>
+      SECTION 8
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style lang="scss">
+
+</style>
